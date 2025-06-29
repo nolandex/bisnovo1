e060c9701e28489eebeee9fa0b738c5325623feb
nolandex <stemapal1234@gmail.com>
2025-06-29 16:16:49 +0700

    Update card.js

--- components/home/feature/card.js
+++ components/home/feature/card.js
@@ -1,11 +1,12 @@
 'use client';
+import React from 'react';
 
 export default function FeatureCard({ featureItem }) {
 	return (
-		<div className="flex flex-col items-center text-center border-2 border-base-content rounded-xl p-5 min-h-[220px] flex-1 shadow hover:shadow-xl transition-all duration-300">
-			<div className="text-4xl mb-2">{<featureItem.icon />}</div>
-			<h3 className="font-bold text-lg">{featureItem.title}</h3>
-			<p className="text-sm opacity-80">{featureItem.description}</p>
+		<div className="flex flex-col items-center justify-start text-center border-2 border-base-content rounded-xl p-5 h-[220px] w-full shadow transition-all duration-300 hover:shadow-xl">
+			<div className="text-3xl mb-2">{<featureItem.icon />}</div>
+			<h3 className="font-bold text-base">{featureItem.title}</h3>
+			<p className="text-sm opacity-80 line-clamp-2">{featureItem.description}</p>
 		</div>
 	);
 }
