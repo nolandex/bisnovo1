ccd9228966ca4dbd482c7750f37b954d863c1cd8
nolandex <stemapal1234@gmail.com>
2025-06-29 16:43:51 +0700

    Update card.js

--- components/home/feature/card.js
+++ components/home/feature/card.js
@@ -4,12 +4,12 @@ import React from 'react';
 export default function FeatureCard({ featureItem = {} }) {
   return (
     <div
-      className='w-full max-w-[300px] p-4 border-2 border-base-content rounded-xl flex flex-col items-center justify-center gap-2 transition-all duration-100 shadow-none hover:shadow-2xl hover:scale-110 bg-base-100 aspect-square'
+      className='w-full max-w-[300px] h-[300px] p-4 border-2 border-base-content rounded-xl flex flex-col items-center justify-center gap-2 transition-all duration-100 shadow-none hover:shadow-2xl hover:scale-110 bg-base-100 aspect-square'
     >
-      <div className="text-center">
+      <div className="text-center flex flex-col items-center justify-center h-full">
         {featureItem.icon && React.createElement(featureItem.icon, { className: 'text-2xl mx-auto' })}
-        <h2 className='mt-2 text-base font-bold'>{featureItem.title}</h2>
-        <p className='text-xs'>{featureItem.description}</p>
+        <h2 className='mt-2 text-base font-bold text-center'>{featureItem.title}</h2>
+        <p className='text-xs text-center overflow-hidden text-ellipsis line-clamp-3'>{featureItem.description}</p>
       </div>
     </div>
   );
