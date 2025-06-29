6cd53effa26ca4b7699dd5089db8ba755fae2718
nolandex <stemapal1234@gmail.com>
2025-06-30 01:21:52 +0700

    Update card.js

--- components/home/testimonial/card.js
+++ components/home/testimonial/card.js
@@ -4,9 +4,12 @@ import Image from 'next/image';
 
 export default function TestimonialCard({ testimonialItem = {}, langName = 'en' }) {
 	return (
-		<div className='bg-base-200 p-6 rounded-lg h-full flex flex-col'>
-			<div className='flex items-center gap-4 mb-4'>
-				<div className='relative w-12 h-12'>
+		// REVISI: Padding diperkecil dari p-6 menjadi p-4
+		<div className='bg-base-200 p-4 rounded-lg h-full flex flex-col'>
+			{/* REVISI: Gap dan margin bawah diperkecil */}
+			<div className='flex items-center gap-3 mb-3'>
+				{/* REVISI: Ukuran avatar diperkecil dari w-12 h-12 menjadi w-10 h-10 */}
+				<div className='relative w-10 h-10 flex-shrink-0'>
 					<Image
 						src={testimonialItem.avatar}
 						alt={testimonialItem.nickname}
@@ -21,11 +24,8 @@ export default function TestimonialCard({ testimonialItem = {}, langName = 'en' 
 			</div>
 
 			<div className='flex-grow'>
-				{/* PERUBAHAN DI SINI: 
-				  Tanda kutip "..." diubah menjadi entitas HTML &ldquo;...&rdquo; 
-				  untuk memperbaiki error build.
-				*/}
-				<p className='text-sm text-base-content/80 leading-relaxed'>
+				{/* REVISI: Jarak antar baris teks diubah dari relaxed menjadi normal */}
+				<p className='text-sm text-base-content/80 leading-normal'>
 					&ldquo;{testimonialItem.content}&rdquo;
 				</p>
 			</div>
