56781b0457c26f46a51158fbdda4cf882183c2a7
nolandex <stemapal1234@gmail.com>
2025-06-30 17:47:26 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,21 @@
 import './globals.css';
 import { Plus_Jakarta_Sans } from 'next/font/google';
-import ThemeScript from '@/components/common/themeScript';
 
 const jakarta = Plus_Jakarta_Sans({
-	weight: ['500', '800'],
-	subsets: ['latin'],
+  weight: ['500', '800'],
+  subsets: ['latin'],
 });
 
 export default function RootLayout({ children }) {
-	const fontClass = jakarta.className || ''  // 确保有默认值
-	
-	return (
-		<html lang="en" className={fontClass}>
-			<body>
-				<ThemeScript />
-				<div className='w-full min-h-svh text-base-content bg-base-100'>
-					{children}
-				</div>
-			</body>
-		</html>
-	);
+  const fontClass = jakarta.className || '';
+
+  return (
+    <html lang="en" data-theme="corporate" className={fontClass}>
+      <body>
+        <div className="w-full min-h-svh text-base-content bg-base-100">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
 }
